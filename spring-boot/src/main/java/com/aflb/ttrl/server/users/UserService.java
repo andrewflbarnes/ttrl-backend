66d5442763c407e3445903a290cf63a14428dd24
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2020-11-25 00:33:50 +0000

    Cleaned up a redundant annotation

--- spring-boot/src/main/java/com/aflb/ttrl/server/users/UserService.java
+++ spring-boot/src/main/java/com/aflb/ttrl/server/users/UserService.java
@@ -1,7 +1,6 @@
 package com.aflb.ttrl.server.users;
 
 import com.aflb.ttrl.api.server.model.UserItem;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,7 +10,6 @@ public class UserService {
 
     private final UserDao userDao;
 
-    @Autowired
     public UserService(UserDao userDao) {
         this.userDao = userDao;
     }
